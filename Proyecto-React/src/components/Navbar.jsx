eaaec61cc6db4edd0d885a276fb9cfe1341894f9
Reynaldo Pereira <reynaldopereira27@gmail.com>
2023-11-28 16:04:31 -0300

    navbar implementado

---
+++ Proyecto-React/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export function NavBar() {
+
+    return (
+        <nav>
+            <ul>
+                <li>Franelas</li>
+                <li>Buzos</li>
+                <li>Shorts</li>
+                <li>Pantalones</li>
+                <li>Zapatos</li>
+                <li>Carrito</li>
+            </ul>
+        </nav>
+    )
+}
